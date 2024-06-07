c7a32265b7deadf275516a8b90e49f1ccc5e722d
Kyu-seok <Kyu-seok@users.noreply.github.com>
2024-06-07 13:23:19 +0000

    deploy: 7c5b1c69348c984103d5b0f37060b78121c1fff1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Test Draft Markdown post",
+        "excerpt":"Test draft   Hello world   TODO List      todo 1   todo 2   todo 3   todo 4   numberings      here   is the   test   numberings  ","categories": ["software-engineering"],
+        "tags": [],
+        "url": "/software-engineering/test_draft/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      }]
