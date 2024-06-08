419391c57cf96949477102c03774951cce25c77b
Kyu-seok <Kyu-seok@users.noreply.github.com>
2024-06-08 08:25:55 +0000

    deploy: 9861c0342189a8b34c2ef9f50bc9333d4b89896b

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
         "title": "Test Draft Markdown post",
-        "excerpt":"Test draft   Hello world   TODO List      todo 1   todo 2   todo 3   todo 4   numberings      here   is the   test   numberings  ","categories": ["software-engineering"],
-        "tags": [],
-        "url": "/software-engineering/test_draft/",
+        "excerpt":"Test draft   Hello world   TODO List      todo 1   todo 2   todo 3   todo 4   numberings      here   is the   test   numberings  ","categories": ["독후감","일상","life"],
+        "tags": ["test","software","java"],
+        "url": "/%EB%8F%85%ED%9B%84%EA%B0%90/%EC%9D%BC%EC%83%81/life/test_draft/",
         "teaser": null
       },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Title of Your Post",
+        "excerpt":"Test Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – OrderedPermalink ordered item ordered item unordered unordered ordered item ordered item ordered...","categories": [],
+        "tags": [],
+        "url": "/test-wide/",
         "teaser": null
       }]
